792573efa8cd1c3ad8a00f0ee6978bd4941e1c21
Self <bhubaneswar.behera@gmail.com>
2025-04-29 19:18:01 +0530

    maxSubarraySum or Kadanes Algorithm

---
+++ src/com/love/babbar/dsa/arrays/MaxSubarraySumOrKadanesAlgorithm.java
@@ -0,0 +1,33 @@
+package com.love.babbar.dsa.arrays;
+
+/**
+ *
+ * https://www.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+ * maxSubarraySum or Kadanes Algorithm
+ *
+ */
+public class MaxSubarraySumOrKadanesAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, -8, 7, -1, 2, 3};
+        int result = maxSubarraySum(arr);
+        System.out.println("Maximum subarray sum is: " + result);
+    }
+
+    public static int maxSubarraySum(int[] arr) {
+        int n = arr.length;
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < n ; i++) {
+            currentSum = currentSum + arr[i];
+            if (currentSum > maxSum){
+                maxSum = currentSum;
+            }
+            if (currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+
+    }
+}
