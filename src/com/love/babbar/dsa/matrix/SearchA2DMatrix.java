8ab1ee066837d5be46100a4ea7015336f7ae69d9
Self <bhubaneswar.behera@gmail.com>
2025-05-06 17:27:38 +0530

    Matrix

--- src/com/love/babbar/dsa/matrix/SearchA2DMatrix.java
+++ src/com/love/babbar/dsa/matrix/SearchA2DMatrix.java
@@ -44,18 +44,18 @@ public class SearchA2DMatrix {
         return -1;
     }
 
-    private static boolean binarySearchOverRow(int rowIdx, int[][] matrix, int target) {
+    private static boolean binarySearchOverRow(int rowNumber, int[][] matrix, int target) {
         int low = 0;
-        int high = matrix[rowIdx].length - 1;
+        int high = matrix[rowNumber].length - 1;
         while (low <= high) {
             int mid = low + (high - low)/2;
 
-            if (matrix[rowIdx][mid] == target) {
+            if (matrix[rowNumber][mid] == target) {
                 return true;
-            } else if (matrix[rowIdx][mid] > target){
-                high = mid - 1;
-            } else{
+            } else if (matrix[rowNumber][mid] < target){
                 low = mid + 1;
+            } else if (matrix[rowNumber][mid] > target) {
+                high = mid - 1;
             }
         }
         return false;
