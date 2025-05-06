816aa2c02088e52187746a032fbdf6ee92eab1ed
Self <bhubaneswar.behera@gmail.com>
2025-05-06 08:33:55 +0530

    Matrix

--- src/com/love/babbar/dsa/matrix/SearchA2DMatrix.java
+++ src/com/love/babbar/dsa/matrix/SearchA2DMatrix.java
@@ -6,7 +6,18 @@ package com.love.babbar.dsa.matrix;
  * Reference : https://www.youtube.com/watch?v=RhPqIIOUiS8
  */
 public class SearchA2DMatrix {
-    boolean searchMatrix(int[][] matrix, int target) {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 50},
+                {60, 70, 80, 90}
+        };
+    int target = 3;
+
+    System.out.println("Result: " + searchMatrix(matrix,target)); // Output: true
+    }
+   public static boolean searchMatrix(int[][] matrix, int target) {
 
         int rowIdx = searchPotentialRow(matrix, target);
         if (rowIdx != -1) {
@@ -15,27 +26,25 @@ public class SearchA2DMatrix {
         return false;
     }
 
-    private int searchPotentialRow(int[][] matrix, int target) {
+    private static int searchPotentialRow(int[][] matrix, int target) {
         int low = 0;
         int high = matrix.length - 1;
-        int idx = matrix[0].length - 1;
+        int lastColumnIdx = matrix[0].length - 1;
         while (low <= high) {
             int mid = low + (high - low)/2;
 
-            if (matrix[mid][0] <= target && target <= matrix[mid][idx]) {
+            if (matrix[mid][0] <= target && target <= matrix[mid][lastColumnIdx]) {
                 return mid;
-            }
-            else if (matrix[mid][0] < target) {
+            } else if (matrix[mid][0] < target) {
                 low = mid + 1;
-            }
-            else if (matrix[mid][0] > target) {
+            } else if (matrix[mid][0] > target) {
                 high = mid - 1;
             }
         }
         return -1;
     }
 
-    private boolean binarySearchOverRow(int rowIdx, int[][] matrix, int target) {
+    private static boolean binarySearchOverRow(int rowIdx, int[][] matrix, int target) {
         int low = 0;
         int high = matrix[rowIdx].length - 1;
         while (low <= high) {
@@ -43,11 +52,9 @@ public class SearchA2DMatrix {
 
             if (matrix[rowIdx][mid] == target) {
                 return true;
-            }
-            else if (matrix[rowIdx][mid] > target){
+            } else if (matrix[rowIdx][mid] > target){
                 high = mid - 1;
-            }
-            else{
+            } else{
                 low = mid + 1;
             }
         }
