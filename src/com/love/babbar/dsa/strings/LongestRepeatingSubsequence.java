ad85d1ee4f70060c25f8f560d03e26d50db31f3c
Self <bhubaneswar.behera@gmail.com>
2025-05-08 22:41:30 +0530

    LongestRepeatingSubsequence

---
+++ src/com/love/babbar/dsa/strings/LongestRepeatingSubsequence.java
@@ -0,0 +1,17 @@
+package com.love.babbar.dsa.strings;
+
+/**
+ *
+ * https://www.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1
+ */
+public class LongestRepeatingSubsequence {
+
+    public static void main(String[] args) {
+        String s = "axxzxy";
+        //System.out.println(LongestRepeatingSubsequence(s));
+    }
+   /* public static int LongestRepeatingSubsequence(String s) {
+        // code here
+
+    }*/
+}
