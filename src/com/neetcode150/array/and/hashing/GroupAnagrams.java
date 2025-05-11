b11fddba391879921df5bd27dc0389128de3557c
Self <bhubaneswar.behera@gmail.com>
2025-05-11 11:58:10 +0530

    Group Anagrams

---
+++ src/com/neetcode150/array/and/hashing/GroupAnagrams.java
@@ -0,0 +1,35 @@
+package com.neetcode150.array.and.hashing;
+
+
+import java.util.*;
+
+/**
+ *
+ * https://neetcode.io/problems/anagram-groups
+ */
+
+public class GroupAnagrams {
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            // Sort the string to create a key
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            // Add the string to the corresponding group
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        // Return all grouped anagrams
+        return new ArrayList<>(map.values());
+    }
+}
