34edc61bfb944c5a1ca2696afee26770e74a17ae
Self <bhubaneswar.behera@gmail.com>
2025-05-12 21:47:54 +0530

    EvaluateReversePolishNotation

---
+++ src/com/neetcode150/stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,43 @@
+package com.neetcode150.stack;
+
+import java.util.Stack;
+
+/**
+ *
+ * https://neetcode.io/problems/evaluate-reverse-polish-notation
+ */
+public class EvaluateReversePolishNotation {
+
+    public static void main(String[] args) {
+        String[] tokens = {"1","2","+","3","*","4","-"};
+        System.out.println(evalRPN(tokens));
+        // Output: 5
+        // Explanation: ((1 + 2) * 3) - 4 = 5
+    }
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            // Check if token is an operator
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = 0;
+
+                switch (token) {
+                    case "+": result = a + b; break;
+                    case "-": result = a - b; break;
+                    case "*": result = a * b; break;
+                    case "/": result = a / b; break; // Integer division
+                }
+
+                stack.push(result);
+            } else {
+                // Token is a number
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+}
