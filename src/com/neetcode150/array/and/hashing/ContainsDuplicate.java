b6c8bef164c52fc4157ea242b4db576e33c6909f
Self <bhubaneswar.behera@gmail.com>
2025-05-22 19:03:59 +0530

    ContainsDuplicate

--- src/com/neetcode150/array/and/hashing/ContainsDuplicate.java
+++ src/com/neetcode150/array/and/hashing/ContainsDuplicate.java
@@ -1,6 +1,8 @@
 package com.neetcode150.array.and.hashing;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -12,6 +14,7 @@ public class ContainsDuplicate {
     public static void main(String[] args) {
         int[]  nums = {1, 2, 3, 3};
         System.out.println(hasDuplicate(nums));
+        System.out.println(hasDuplicate1(nums));
     }
     public static boolean hasDuplicate(int[] nums) {
         int n = nums.length;
@@ -24,6 +27,23 @@ public class ContainsDuplicate {
             hashSet.add(num);
         }
 
+        return false;
+    }
+
+    //using HashMap
+    public static boolean hasDuplicate1(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n ; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) +1);
+        }
+
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            if(entry.getValue() > 1){
+                return true;
+            }
+        }
         return false;
     }
 }
