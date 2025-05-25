331ad1e44dd8e5aada842b7f245e882377a109ff
Self <bhubaneswar.behera@gmail.com>
2025-05-26 00:38:10 +0530

    MedianOfTwoSortedArrays

--- src/com/neetcode150/binary/search/MedianOfTwoSortedArrays.java
+++ src/com/neetcode150/binary/search/MedianOfTwoSortedArrays.java
@@ -15,6 +15,7 @@ public class MedianOfTwoSortedArrays {
         //System.out.println(findMedianSortedArraysBruteForce(nums1, nums2));
         System.out.println(findMedianSortedArrays(nums1, nums2));
     }
+    //Time Complexity: O(m+n)) where m and n are the lengths of the two arrays.
     public static double findMedianSortedArraysBruteForce(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
@@ -31,19 +32,27 @@ public class MedianOfTwoSortedArrays {
         }
     }
 
+    // Time Complexity: O(log(min(m, n))) where m and n are the lengths of the two arrays.
     public static double findMedianSortedArrays(int[] arr1, int[] arr2) {
         if(arr2.length < arr1.length) {
             return findMedianSortedArrays(arr2, arr1);
         }
-       int low = 0;
+        int low = 0;
         int high = arr1.length;
 
         while (low <= high ){
             int mid1 = low + (high - low) / 2;
             int mid2 = (arr1.length + arr2.length + 1) / 2 - mid1;//+1 as it works for odd and even
+            // if mid1 is 0, it means there are no elements on the left side of arr1
             int left1 = (mid1 == 0) ? Integer.MIN_VALUE : arr1[mid1 - 1];
+
+            // if mid1 is equal to the length of arr1, it means there are no elements on the right side of arr1
             int right1 = (mid1 == arr1.length) ? Integer.MAX_VALUE : arr1[mid1];
+
+            // if mid2 is 0, it means there are no elements on the left side of arr2
             int left2 = (mid2 == 0) ? Integer.MIN_VALUE : arr2[mid2 - 1];
+
+            // if mid2 is equal to the length of arr2, it means there are no elements on the right side of arr2
             int right2 = (mid2 == arr2.length) ? Integer.MAX_VALUE : arr2[mid2];
 
             if(left1 <= right2 && left2 <= right1) {
