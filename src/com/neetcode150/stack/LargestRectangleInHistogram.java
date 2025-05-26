3b367ca65b75fe86432d8a2c5b2bbbdacdf1ba8a
Self <bhubaneswar.behera@gmail.com>
2025-05-26 20:22:57 +0530

    LargestRectangleInHistogram

--- src/com/neetcode150/stack/LargestRectangleInHistogram.java
+++ src/com/neetcode150/stack/LargestRectangleInHistogram.java
@@ -5,12 +5,13 @@ import java.util.Stack;
 /**
  *
  * https://neetcode.io/problems/largest-rectangle-in-histogram
+ * Reference : https://www.youtube.com/watch?v=ysy1o-QEj3k&t=1650s
  */
 public class LargestRectangleInHistogram {
 
     public static void main(String[] args) {
-        int[] heights = {7,1,7,2,2,4};
-        System.out.println(largestRectangleArea(heights)); // Output: 8
+        int[] heights = {2,4};
+        System.out.println(largestRectangleArea(heights)); // Output: 10
     }
 
     public static int largestRectangleArea(int[] heights) {
@@ -18,19 +19,37 @@ public class LargestRectangleInHistogram {
         int maxArea = 0;
         int n = heights.length;
 
-        for (int i = 0; i <= n; i++) {
-            int currentHeight = (i == n) ? 0 : heights[i];
-
-            while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {
-                int height = heights[stack.pop()];
-                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-
-                maxArea = Math.max(maxArea, height * width);
+        //Right smallest nearest element
+        int[] rightSmallest = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            // If the stack is not empty, we pop elements until we find a smaller element
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
             }
+            // If stack is empty, it means there is no smaller element to the right
+            // we have to use n as the right boundary
+            rightSmallest[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
 
+        //left smallest nearest element
+        int[] leftSmallest = new int[n];
+        for (int i = 0; i < n; i++) {
+            // If the stack is not empty, we pop elements until we find a smaller element
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            leftSmallest[i] = stack.isEmpty() ? -1 : stack.peek();
             stack.push(i);
         }
 
+        // Calculate the maximum area
+        for (int i = 0; i < n ; i++) {
+            int width = rightSmallest[i] - leftSmallest[i] - 1;
+            int currentArea = heights[i] * width;
+            maxArea = Math.max(maxArea, currentArea);
+        }
         return maxArea;
     }
 }
