1d3d1eebd39f35d8582450e00c79d1bf018ce9bc
Self <bhubaneswar.behera@gmail.com>
2025-05-28 14:26:30 +0530

    LongestRepeatingCharacterReplacement

--- src/com/neetcode150/sliding/window/LongestRepeatingCharacterReplacement.java
+++ src/com/neetcode150/sliding/window/LongestRepeatingCharacterReplacement.java
@@ -20,22 +20,24 @@ public class LongestRepeatingCharacterReplacement {
 
     public static int characterReplacement(String s, int k) {
         Map<Character, Integer> freqMap = new HashMap<>();
-        int maxFreq = 0, left = 0, maxWindow = 0;
+        int maxFreq = 0;
+        int left = 0;
+        int maxWindow = 0;
 
         for (int right = 0; right < s.length(); right++) {
             char currentChar = s.charAt(right);
             freqMap.put(currentChar, freqMap.getOrDefault(currentChar, 0) + 1);
 
             maxFreq = Math.max(maxFreq, freqMap.get(currentChar));
-            int windowLength = right - left + 1;
+            int windowSize = right - left + 1;
 
-            if (windowLength - maxFreq > k) {
+            if (windowSize - maxFreq > k) {
                 char leftChar = s.charAt(left);
                 freqMap.put(leftChar, freqMap.get(leftChar) - 1);
                 left++;
             }
 
-            maxWindow = Math.max(maxWindow, windowLength);
+            maxWindow = Math.max(maxWindow, windowSize);
         }
         return maxWindow;
     }
