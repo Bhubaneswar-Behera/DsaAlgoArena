8f6cc37142d56bf14e84f70a5265593b43eb50e5
Self <bhubaneswar.behera@gmail.com>
2025-06-02 19:58:18 +0530

    ReorderLinkedList

---
+++ src/com/neetcode150/linkedlist/ReorderLinkedList.java
@@ -0,0 +1,20 @@
+package com.neetcode150.linkedlist;
+
+public class ReorderLinkedList {
+
+    class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void reorderList(ListNode head) {
+
+    }
+}
