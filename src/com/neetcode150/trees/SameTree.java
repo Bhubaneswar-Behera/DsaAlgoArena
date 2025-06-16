dbd3247dc8c959d842ba4c4d4c6317bb40590a2d
Self <bhubaneswar.behera@gmail.com>
2025-06-16 21:30:33 +0530

    SameTree

---
+++ src/com/neetcode150/trees/SameTree.java
@@ -0,0 +1,42 @@
+package com.neetcode150.trees;
+
+/**
+ *
+ * https://leetcode.com/problems/same-tree/description/
+ */
+public class SameTree {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // If both nodes are null, they are the same
+        if (p == null && q == null) {
+            return true;
+        }
+        // If one of the nodes is null, they are not the same
+        if (p == null || q == null) {
+            return false;
+        }
+        // Check if the current nodes have the same value
+        if (p.val != q.val) {
+            return false;
+        }
+        // Recursively check the left and right subtrees
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
