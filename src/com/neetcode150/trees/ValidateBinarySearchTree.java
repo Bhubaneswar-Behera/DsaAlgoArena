e8144b11ea2eb95f01efa2984b81d5064627ed9d
Self <bhubaneswar.behera@gmail.com>
2025-06-17 22:19:10 +0530

    ValidateBinarySearchTree

---
+++ src/com/neetcode150/trees/ValidateBinarySearchTree.java
@@ -0,0 +1,61 @@
+package com.neetcode150.trees;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ * Reference : https://www.youtube.com/watch?v=sLoZJ2E4ZDs
+ */
+public class ValidateBinarySearchTree {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public static void main(String[] args) {
+
+    }
+    //Time Complexity: O(n)
+    //Space Complexity: O(n)
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> inOrderList = new LinkedList<>();
+
+        // Populate the list
+        helper(root, inOrderList);
+
+        boolean isBST = true;
+        int prev = inOrderList.get(0);
+        for (int i = 1; i < inOrderList.size(); i++) {
+
+            // Check if new element is smaller than previous element
+            // or if the element is duplicate
+            if (inOrderList.get(i) <= prev) {
+                isBST = false;
+            }
+
+            prev = inOrderList.get(i);
+        }
+
+        return isBST;
+    }
+
+    void helper(TreeNode treeNode, List<Integer> inOrderList) {
+        if (treeNode == null) {
+            return;
+        }
+
+        helper(treeNode.left, inOrderList);
+        inOrderList.add(treeNode.val);
+        helper(treeNode.right, inOrderList);
+    }
+}
