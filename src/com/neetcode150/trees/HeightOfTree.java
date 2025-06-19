b044b432d94681363fea6f0730f0d3a0fa959f3e
Self <bhubaneswar.behera@gmail.com>
2025-06-19 22:32:58 +0530

    Trees

--- src/com/neetcode150/trees/HeightOfTree.java
+++ src/com/neetcode150/trees/HeightOfTree.java
@@ -2,18 +2,6 @@ package com.neetcode150.trees;
 
 public class HeightOfTree {
 
-    static class Node {
-        int data;
-        Node left;
-        Node right;
-
-        Node(int data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
-    }
-
     public static void main(String[] args) {
 
     }
--- src/com/neetcode150/trees/SumOfNodes.java
+++ src/com/neetcode150/trees/SumOfNodes.java
@@ -2,17 +2,6 @@ package com.neetcode150.trees;
 
 public class SumOfNodes {
 
-    static class Node {
-        int data;
-        Node left;
-        Node right;
-
-        Node(int data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
-    }
     public static void main(String[] args) {
 
     }
