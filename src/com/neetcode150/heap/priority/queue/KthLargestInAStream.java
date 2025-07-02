0b7ea3b29b834ececd2000e51061c3ba3c5360a2
Self <bhubaneswar.behera@gmail.com>
2025-07-02 23:06:50 +0530

    Priority Queue

--- src/com/neetcode150/heap/priority/queue/KthLargestInAStream.java
+++ src/com/neetcode150/heap/priority/queue/KthLargestInAStream.java
@@ -7,13 +7,20 @@ import java.util.PriorityQueue;
  * https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
  * Reference : https://www.youtube.com/watch?v=i7Mt70QERw4
  */
-public class KthLargest {
+public class KthLargestInAStream {
     private PriorityQueue<Integer> minHeap;
     private int k;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        // Example usage
+        KthLargestInAStream kthLargest = new KthLargestInAStream(3, new int[]{4, 5, 8, 2});
+        System.out.println(kthLargest.add(3)); // Returns 4
+        System.out.println(kthLargest.add(5)); // Returns 5
+        System.out.println(kthLargest.add(10)); // Returns 5
+        System.out.println(kthLargest.add(9)); // Returns 8
+        System.out.println(kthLargest.add(4)); // Returns 8
     }
-    public KthLargest(int k, int[] nums) {
+    public KthLargestInAStream(int k, int[] nums) {
         this.k = k;
         this.minHeap = new PriorityQueue<>(k); // Min-heap with a capacity of k
 
