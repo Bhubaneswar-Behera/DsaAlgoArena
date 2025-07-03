6daf98e7f7ef707dc80fd4be8248066b51d33a91
Self <bhubaneswar.behera@gmail.com>
2025-07-03 21:33:08 +0530

    MedianFinder

--- src/com/neetcode150/heap/priority/queue/MedianFinder.java
+++ src/com/neetcode150/heap/priority/queue/MedianFinder.java
@@ -11,6 +11,15 @@ public class MedianFinder {
     private PriorityQueue<Integer> maxHeap;
     private PriorityQueue<Integer> minHeap;
 
+
+    public static void main(String[] args) {
+        MedianFinder medianFinder = new MedianFinder();
+        medianFinder.addNum(1);
+        medianFinder.addNum(2);
+        System.out.println(medianFinder.findMedian()); // Output: 1.5
+        medianFinder.addNum(3);
+        System.out.println(medianFinder.findMedian()); // Output: 2.0
+    }
     public MedianFinder() {
         // Max heap for the lower half of the numbers
         maxHeap = new PriorityQueue<>((a, b) -> b - a);
