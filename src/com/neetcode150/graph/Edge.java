f2825f648728110a1e848d0dd4ddf94c68a74731
Self <bhubaneswar.behera@gmail.com>
2025-07-07 22:16:32 +0530

    Graph

---
+++ src/com/neetcode150/graph/Edge.java
@@ -0,0 +1,11 @@
+package com.neetcode150.graph;
+
+public class Edge {
+    public int source;
+    public int destination;
+
+    public Edge(int source, int destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+}
