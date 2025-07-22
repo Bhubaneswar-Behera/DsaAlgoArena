6f321f9d8d65e0d0206ab3bac782216eefca176e
Self <bhubaneswar.behera@gmail.com>
2025-07-22 18:28:25 +0530

    Arrays

--- src/com/neetcode150/array/and/hashing/ProductsOfArrayExceptSelf.java
+++ src/com/neetcode150/array/and/hashing/ProductsOfArrayExceptSelf.java
@@ -23,18 +23,18 @@ public class ProductsOfArrayExceptSelf {
         left[0] = 1;
         for (int i = 1; i < nums.length; i++) {
             left[i] = left[i - 1] * nums[i - 1];
-        }
+        }//after the run output is [1, 1, 2, 8]
 
         right[nums.length - 1] = 1;
         for (int i = nums.length - 2; i >= 0 ; i--) {
             right[i] = right[i + 1] * nums[i + 1];
-        }
+        }//after the run output is [48, 24, 12, 2]
 
         int[] ans = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             ans[i] = left[i] * right[i];
         }
 
-        return ans;
+        return ans; // [48, 24, 12, 8]
     }
 }
