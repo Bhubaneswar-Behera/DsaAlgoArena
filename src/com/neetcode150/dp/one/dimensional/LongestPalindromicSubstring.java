abc014f9c169e35b3986161826d040170f26db00
Self <bhubaneswar.behera@gmail.com>
2025-08-15 12:37:49 +0530

    DP

---
+++ src/com/neetcode150/dp/one/dimensional/LongestPalindromicSubstring.java
@@ -0,0 +1,45 @@
+package com.neetcode150.dp.one.dimensional;
+
+/**
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ */
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+        String s = "anamadam";
+        s = longestPalindrome(s);
+        System.out.println(longestPalindrome(s));
+    }
+
+    public static String longestPalindrome(String s) {
+        // code here
+        int n = s.length();
+        String answer = "";
+
+        for (int i = 0; i < n ; i++) {
+            String oddPalindrome = oddEven(s, i, i);
+            String evenPalindrome = oddEven(s, i, i + 1);
+            String max = oddPalindrome.length() > evenPalindrome.length() ? oddPalindrome : evenPalindrome;
+
+            answer = max.length() > answer.length() ? max : answer;
+        }
+        return answer;
+    }
+
+    public static String oddEven(String str, int start, int end) {
+        int n = str.length();
+        int left = start;
+        int right = end;
+
+        while (left >= 0 && right < n) {
+            if (str.charAt(left) != str.charAt(right)) {
+                break;
+            }
+            left--;
+            right++;
+        }
+        return str.substring(left + 1, right);
+
+    }
+}
