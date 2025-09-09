a5b35a2621a6cf0c63b374d15e7c165b343bdb65
Self <bhubaneswar.behera@gmail.com>
2025-09-09 08:11:40 +0530

    Binary Search

--- src/com/neetcode150/binary/search/SearchA2DMatrix.java
+++ src/com/neetcode150/binary/search/SearchA2DMatrix.java
@@ -3,7 +3,7 @@ package com.neetcode150.binary.search;
 /**
  *
  * https://leetcode.com/problems/search-a-2d-matrix/submissions/1644073156/
- * Reference : https://www.youtube.com/watch?v=RhPqIIOUiS8&t=637s
+ *
  */
 public class SearchA2DMatrix {
 
@@ -18,45 +18,23 @@ public class SearchA2DMatrix {
     }
 
     public static boolean searchMatrix(int[][] matrix, int target) {
-        int rowIdx = searchPotentialRow(matrix, target);
-        if (rowIdx != -1) {
-            return binarySearchOverRow(rowIdx, matrix, target);
-        }
-        return false;
-    }
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int i = 0;
+        int j = m - 1;
 
-    private static int searchPotentialRow(int[][] matrix, int target) {
-        int firstRowIndex = 0;
-        int lastRowIndex = matrix.length - 1;
-        int lastColIndex = matrix[0].length - 1;
-        while (firstRowIndex <= lastRowIndex) {
-            int mid = firstRowIndex + (lastRowIndex - firstRowIndex)/2;
-
-            if (matrix[mid][0] <= target && target <= matrix[mid][lastColIndex]) {
-                return mid;
-            } else if (target > matrix[mid][0]) {
-                firstRowIndex = mid + 1;
-            } else if (target < matrix[mid][0]) {
-                lastRowIndex = mid - 1;
-            }
-        }
-        return -1;
-    }
-
-    private static boolean binarySearchOverRow(int rowIdx, int[][] matrix, int target) {
-        int left = 0;
-        int right = matrix[rowIdx].length - 1;
-        while (left <= right) {
-            int mid = left + (right - left)/2;
-
-            if (matrix[rowIdx][mid] == target) {
+        while(i < n && j >= 0){
+            if(matrix[i][j] == target){
                 return true;
-            } else if (target > matrix[rowIdx][mid] ) {
-                left = mid + 1;
-            } else if(target < matrix[rowIdx][mid]){
-                right = mid - 1;
+            }
+            if ( target > matrix[i][j]){
+                i++;
+            } else{
+                j--;
             }
         }
         return false;
     }
+
+
 }
