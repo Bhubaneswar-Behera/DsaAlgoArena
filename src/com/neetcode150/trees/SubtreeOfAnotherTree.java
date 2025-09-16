9231710182dcdb7b869aad61960186d450520812
Self <bhubaneswar.behera@gmail.com>
2025-09-16 17:58:01 +0530

    Tree

--- src/com/neetcode150/trees/SubtreeOfAnotherTree.java
+++ src/com/neetcode150/trees/SubtreeOfAnotherTree.java
@@ -7,6 +7,21 @@ package com.neetcode150.trees;
 public class SubtreeOfAnotherTree {
 
     public static void main(String[] args) {
+        // Example usage
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(0);
+
+        TreeNode subRoot = new TreeNode(4);
+        subRoot.left = new TreeNode(1);
+        subRoot.right = new TreeNode(2);
+
+        SubtreeOfAnotherTree solution = new SubtreeOfAnotherTree();
+        boolean result = solution.isSubtree(root, subRoot);
+        System.out.println("Is subtree: " + result); // Should print true
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if (subRoot == null) {
