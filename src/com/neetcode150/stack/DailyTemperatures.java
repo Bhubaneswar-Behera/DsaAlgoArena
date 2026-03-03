b011ac3f91f5956eef146928b928cf39a85c5329
Self <bhubaneswar.behera@gmail.com>
2026-03-03 17:58:29 +0530

    Arrays

--- src/com/neetcode150/stack/DailyTemperatures.java
+++ src/com/neetcode150/stack/DailyTemperatures.java
@@ -11,8 +11,9 @@ import java.util.Stack;
 public class DailyTemperatures {
 
     public static void main(String[] args) {
-        int[] temperatures = {30,38,30,36,35,40,28};
+        int[] temperatures = {73,74,75,71,69,72,76,73};
         System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
+        //Output: [1,1,4,2,1,1,0,0]
 
     }
 
