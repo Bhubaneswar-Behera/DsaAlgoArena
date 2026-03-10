29df6601965e9c151e232da0e601a7d495b109b1
Self <bhubaneswar.behera@gmail.com>
2026-03-10 22:05:47 +0530

    Arrays

--- src/com/leetcode/journey/arrays/MergeIntervals.java
+++ src/com/leetcode/journey/arrays/MergeIntervals.java
@@ -18,6 +18,7 @@ public class MergeIntervals {
             System.out.println(Arrays.toString(interval));
         }
     }
+    //Time Co
     public static int[][] merge(int[][] intervals) {
         // Step 1: Sort intervals by start time
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
@@ -26,11 +27,11 @@ public class MergeIntervals {
 
         // Step 2: Merge intervals
         for (int[] interval : intervals) {
-            // If the list is empty or the current interval does not overlap with the last one
+            // If the list is empty or If last interval end < current interval start → no overlap
             if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
                 merged.add(interval);
             } else {
-                // Overlapping intervals, merge them
+                // Overlapping intervals, merge them i.e. Update the end of last interval with the maximum end value.
                 merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
             }
         }
