5166c0c5b1f5509fdef5878cbc73299e02868edc
Self <bhubaneswar.behera@gmail.com>
2026-05-12 09:05:14 +0530

    Array and Hashing

--- src/com/neetcode150/binary/search/KokoEatingBananas.java
+++ src/com/neetcode150/binary/search/KokoEatingBananas.java
@@ -5,6 +5,22 @@ package com.neetcode150.binary.search;
  * https://neetcode.io/problems/eating-bananas
  * Reference: https://www.youtube.com/watch?v=JGYXNpZaW2U
  */
+/**
+ * Finds the minimum eating speed required to finish all banana piles
+ * within the given number of hours.
+ *
+ * Approach:
+ * - Use Binary Search on possible eating speeds.
+ * - Minimum possible speed = 1 banana/hour.
+ * - Maximum possible speed = largest pile size.
+ *
+ * Time Complexity:
+ * O(n * log(maxPile))
+ *
+ * Space Complexity:
+ * O(1)
+ *
+ */
 public class KokoEatingBananas {
 
     public static void main(String[] args) {
@@ -13,40 +29,65 @@ public class KokoEatingBananas {
         System.out.println(minEatingSpeed(piles, hours));
     }
     public static int minEatingSpeed(int[] piles, int hours) {
-        int low = 1;
+        // Minimum possible eating speed
+        int minimumSpeed = 1;
 
-        // Find max pile size
-        int high = 0;
-        for (int pile : piles){
-            high = Math.max(high, pile);
+        // Maximum possible eating speed
+        int maximumSpeed = 0;
+
+        // Find the largest pile size
+        for (int pileSize : piles) {
+            maximumSpeed = Math.max(maximumSpeed, pileSize);
         }
 
-        // Binary search
-        while (low < high) {
-            int mid = low + (high - low) / 2;
+        // Perform Binary Search on eating speed
+        while (minimumSpeed < maximumSpeed) {
 
-            if (canEatInTime(piles, hours, mid)) {
-                high = mid; // try smaller speed
-            }
-            else {
-                low = mid + 1; // need bigger speed
+            int currentSpeed = minimumSpeed + (maximumSpeed - minimumSpeed) / 2;
+
+            // Check whether Koko can finish all piles
+            // within the allowed hours at current speed
+            if (canEatInTime(piles, hours, currentSpeed)) {
+                // Try smaller speed
+                maximumSpeed = currentSpeed;
+
+            } else {
+                // Increase speed because current speed is too slow
+                minimumSpeed = currentSpeed + 1;
             }
         }
-        return low;
+
+        return minimumSpeed;
     }
 
     /**
+     * Checks whether Koko can finish all banana piles
+     * within the given hours using the provided speed.
      *
-     * If Koko eats at speed bananas per hour, can she finish all piles within h hours?
+     * Formula:
+     * Required Hours =
+     * ceil(pileSize / speed)
      *
+     * Example:
+     * pile = 10, speed = 3
+     * required hours = 4
+     *
+     * @param piles banana pile array
+     * @param inputHours allowed hours
+     * @param speed bananas eaten per hour
+     * @return true if all piles can be finished in time
      */
     private static boolean canEatInTime(int[] piles, int inputHours, int speed) {
-        int hours = 0;
-        for(int pile : piles) {
-            hours = hours + (int) Math.ceil((double) pile / speed);
+        int totalHoursRequired = 0;
+
+        for (int pileSize : piles) {
+
+            // Ceiling division to calculate hours required
+            totalHoursRequired += (int) Math.ceil((double) pileSize / speed);
         }
 
-        return hours <= inputHours;
+        return totalHoursRequired <= inputHours;
+    }
     }
 
-}
+
