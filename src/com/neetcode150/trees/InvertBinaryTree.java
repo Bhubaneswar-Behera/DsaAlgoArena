1e3b01058fb7c04379cb79b8897495569f1c3555
Self <bhubaneswar.behera@gmail.com>
2026-05-14 09:43:04 +0530

    Array and Hashing

--- src/com/neetcode150/trees/InvertBinaryTree.java
+++ src/com/neetcode150/trees/InvertBinaryTree.java
@@ -6,7 +6,36 @@ package com.neetcode150.trees;
  */
 public class InvertBinaryTree {
 
-    public TreeNode invertTree(TreeNode root) {
+    public static void main(String[] args) {
+        /*
+                 4
+               /   \
+              2     7
+             / \   / \
+            1   3 6   9
+        */
+
+        TreeNode root = new TreeNode(4);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        System.out.println("Before Invert:");
+        inorder(root);
+
+        invertTree(root);
+
+        System.out.println("\nAfter Invert:");
+        inorder(root);
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
         }
@@ -20,5 +49,16 @@ public class InvertBinaryTree {
         root.right = left;
 
         return root;
+    }
+
+    // Inorder Traversal
+    public static void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
     }
 }
