651dd47a615ed7d863bb71f6b2f5346bda26aac7
Self <bhubaneswar.behera@gmail.com>
2026-05-14 09:45:24 +0530

    Array and Hashing

--- src/com/neetcode150/trees/MaximumDepthOfBinaryTree.java
+++ src/com/neetcode150/trees/MaximumDepthOfBinaryTree.java
@@ -6,7 +6,32 @@ package com.neetcode150.trees;
  */
 public class MaximumDepthOfBinaryTree {
 
-    public int maxDepth(TreeNode root) {
+    public static void main(String[] args) {
+
+        /*
+                 1
+               /   \
+              2     3
+             / \
+            4   5
+
+            Max Depth = 3
+        */
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        int depth = maxDepth(root);
+
+        System.out.println("Maximum Depth of Tree: " + depth);
+    }
+
+    public static int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
